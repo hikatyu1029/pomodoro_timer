1ce013e5f744cd2670291aeef87a35af371316bc
Yori <hikatyu1029@gmail.com>
2022-04-24 23:37:31 +0900

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Simple Pomodoro Timer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
